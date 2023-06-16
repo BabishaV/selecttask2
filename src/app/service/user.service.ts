7be83bf132bfa290f615f83be9ce5e0e5bc676e2
BabishaV <babisha1581@gmail.com>
2023-06-16 18:24:12 +0530

    updated

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -22,6 +22,7 @@ export class UserService {
       return this.http.post(url, userData);
     }
     
+    updateUser( id:any,userData: any) {
       // const url = 'https://gorest.co.in/public/v2/users/?access-token=9def0d50c71e0d10ccdeea8496d2e14c77b34f6302fa03abef7f1de870b982fd';
       const url ='https://gorest.co.in/public/v2/users/'+id+'?access-token=9def0d50c71e0d10ccdeea8496d2e14c77b34f6302fa03abef7f1de870b982fd';
       // Patch request
@@ -35,39 +36,4 @@ export class UserService {
        return this.http.delete(url,id);
   }
   }
-
-//   private apiUrl = 'https://gorest.co.in/public/v2/users';
-//   private token = '9def0d50c71e0d10ccdeea8496d2e14c77b34f6302fa03abef7f1de870b982fd';
-
-//   constructor(private http: HttpClient) { }
-// //get
-//   getUserList() {
-//     return this.http.get<any[]>(this.apiUrl);
-//   }
-// //post
-//   addUser(user: any) {
-//     const headers = new HttpHeaders({
-//       'Content-Type': 'application/json',
-//       'Authorization': `Bearer ${this.token}`
-//     });
-
-//     return this.http.post(this.apiUrl, user, { headers });
-//   }
-   
-  //   updateUser(userId: number, user: any) {
-  //     const headers = new HttpHeaders({
-  //       'Content-Type': 'application/json',
-  //       'Authorization': `Bearer ${this.token}`
-  //     });
-    
-  //     const url = `${this.apiUrl}/${userId}`;
-    
-  //     return this.http.put(url, user, { headers });
-  //   }
-  //  }
-  // deleteItem(userId: number) {
-  //   const url = `${this.apiUrl}/${userId}`;
-  //   return this.http.delete(url);
-  // }
-  
 
